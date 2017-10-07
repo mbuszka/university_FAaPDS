f4b71c2592625f2ea8eadc6b8ffea2611a1278d7
Maciej Buszka <maciej@buszka.eu>
2017-10-07 20:48:53 +0200

    fixed whitespace

--- ex-1.ml
+++ ex-1.ml
@@ -2,25 +2,25 @@
 
 (* Suboptimal solution *)
 fun sublist (x::xs) = 
-	let
-		fun addx (ys::yss) = (x::ys) :: addx yss 
-			| addx nil = nil
-		val xss = sublist xs
-	in (* Here the whole xss gets rebuilt *)
-		xss @ addx xss
-	end
-	| sublist [] = [[]]
+    let
+      fun addx (ys::yss) = (x::ys) :: addx yss 
+        | addx nil = nil
+      val xss = sublist xs
+    in (* Here the whole xss gets rebuilt *)
+      xss @ addx xss
+    end
+  | sublist [] = [[]]
 
 (* Better solution *)
 fun sublist2 (x::xs) = 
-	let
-		fun add_merge (zss, ys::yss) = (x::ys) :: add_merge (zss, yss)
-			| add_merge (zss, []) = zss
-		val xss = sublist2 xs
-		in
-			add_merge (xss, xss)
-		end
-	| sublist2 [] = [[]]
+    let
+      fun add_merge (zss, ys::yss) = (x::ys) :: add_merge (zss, yss)
+        | add_merge (zss, []) = zss
+      val xss = sublist2 xs
+      in
+        add_merge (xss, xss)
+      end
+  | sublist2 [] = [[]]
 
 
 (* Part 2 *)
@@ -32,25 +32,25 @@ val ex = 4 & (2 & (1 & (%, %), 3 & (%, %)), 5 & (%, %))
 
 (* Suboptimal solution *)
 fun flatten (x & (t1, t2)) = flatten t1 @ [x] @ flatten t2
-	| flatten % = nil
+  | flatten % = nil
 
 (* Better solution *)
 fun flatten2 t =
-	let 
-		fun aux (x & (t1, t2), rest) = aux (t1, x :: aux (t2, rest))
-			| aux (%, rest) = rest
-	in
-		aux (t, [])
-	end
+    let 
+      fun aux (x & (t1, t2), rest) = aux (t1, x :: aux (t2, rest))
+        | aux (%, rest) = rest
+    in
+      aux (t, [])
+    end
 
 (* Part 3 *)
 fun rev (x::xs) = rev xs @ [x]
-	| rev [] = []
+  | rev [] = []
 
 fun rev2 ys =
-	let
-		fun aux (acc, x::xs) = aux (x::acc, xs)
-			| aux (acc, []) = acc
-	in
-		aux ([], ys)
-	end
+    let
+      fun aux (acc, x::xs) = aux (x::acc, xs)
+        | aux (acc, []) = acc
+    in
+      aux ([], ys)
+    end
--- ex-3.ml
+++ ex-3.ml
@@ -1,17 +1,17 @@
 fun mergesort [] = []
-	| mergesort [x] = [x]
-	| mergesort ls =
-	let
-		fun split (acc1, acc2, x::y::xs) = split (x::acc1, y::acc2, xs)
-			| split (acc1, acc2, [x]) = (x::acc1, acc2)
-			| split (acc1, acc2, []) = (acc1, acc2)
+  | mergesort [x] = [x]
+  | mergesort ls =
+    let
+      fun split (acc1, acc2, x::y::xs) = split (x::acc1, y::acc2, xs)
+        | split (acc1, acc2, [x]) = (x::acc1, acc2)
+        | split (acc1, acc2, []) = (acc1, acc2)
 
-		fun merge (x::xs, y::ys) = if (x < y) 
-			then x :: merge (xs, y::ys) else y :: merge (x::xs, ys)
-			| merge ([], ys) = ys
-			| merge (xs, []) = xs
+      fun merge (x::xs, y::ys) = if (x < y) 
+            then x :: merge (xs, y::ys) else y :: merge (x::xs, ys)
+        | merge ([], ys) = ys
+        | merge (xs, []) = xs
 
-		val (xs, ys) = split ([], [], ls)
-	in
-		merge (mergesort xs, mergesort ys)
-	end
+      val (xs, ys) = split ([], [], ls)
+    in
+      merge (mergesort xs, mergesort ys)
+    end
