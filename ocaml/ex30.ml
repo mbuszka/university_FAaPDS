f571d35ba9657b97be92177f5e0db23a3ee81e1c
Maciej Buszka <maciej@buszka.eu>
2017-11-13 11:49:43 +0100

    ex 30

--- ocaml/ex30.ml
+++ ocaml/ex30.ml
@@ -1,1 +1,20 @@
-open LzyStream
+module type MyLazy =
+sig
+  type 'a t
+
+  val delay: (unit -> 'a) -> 'a t
+  val force: 'a t -> 'a
+end
+
+module MyLazy: MyLazy =
+struct
+  type 'a susp_state = Thunk of (unit -> 'a)
+                     | Value of 'a
+  type 'a t = { mutable state: 'a susp_state }
+
+  let delay f = { state = Thunk f }
+  let force r =
+    match r.state with
+    | Thunk f -> let x = f () in r.state <- Value x; x
+    | Value x -> x
+end
